344cb5f5cb58d2afab74946ad1aab9684da86a65
Stanislav Takt <st.takt@gmail.com>
2026-09-18 08:42:53 +0300

    style: normalize model imports

--- orbitas_connector/models/__init__.py
+++ orbitas_connector/models/__init__.py
@@ -1,6 +1,8 @@
-from . import account_move
-from . import orbitas_backend
-from . import orbitas_binding
-from . import orbitas_clearing
-from . import orbitas_inbound
-from . import orbitas_outbox
+from . import (
+    account_move,
+    orbitas_backend,
+    orbitas_binding,
+    orbitas_clearing,
+    orbitas_inbound,
+    orbitas_outbox,
+)
